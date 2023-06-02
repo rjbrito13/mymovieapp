e2a8fbd819087d1afd3b58fc4f88d230bc420963
rjbrito13 <royjamesbrito@gmail.com>
2023-06-03 02:45:43 +0800

    modify main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme:  ColorScheme.fromSeed(seedColor: Colors.blue,brightness: Brightness.dark),
         useMaterial3: true,
       ),
-      home: const MovieHome(title: "Hello, Roy",),
+      home: const MovieHome(title: "Hello, trisha",),
     );
   }
 }
